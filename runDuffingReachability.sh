90caa835fd665de3a4575b3b6f1cf0423599f1fc
Hunter Quebedeaux <hunter.quebedeaux@ucf.edu>
2026-02-12 12:47:35 -0500

    added full script to run comprehensive results

--- runDuffingReachability.sh
+++ runDuffingReachability.sh
@@ -1,11 +1,28 @@
 #!/bin/bash
 
-python scripts/reachabilityDuffing.py --train-ratio 0.2 --model mamba 
-python scripts/reachabilityDuffing.py --train-ratio 0.8 --model mamba 
-python scripts/reachabilityDuffing.py --train-ratio 0.2 --model mamba --ood
-python scripts/reachabilityDuffing.py --train-ratio 0.8 --model mamba --ood
+# default parameters for dataset generation
+DV=0.3
+DV_ood=$(awk "BEGIN {print $DV*2}")
 
-python scripts/reachabilityDuffing.py --train-ratio 0.2 --model lstm 
-python scripts/reachabilityDuffing.py --train-ratio 0.8 --model lstm 
-python scripts/reachabilityDuffing.py --train-ratio 0.2 --model lstm --ood
-python scripts/reachabilityDuffing.py --train-ratio 0.8 --model lstm --ood
+echo "Using delta-v radius: ${DV}"
+echo "Using OOD delta-v radius: ${DV_ood}"
+
+# run dataset generation for the Duffing system if files do not exist
+if [ ! -f "data/test/duffing_monte_carlo_trajectories_dv_${DV}_dt_0.02_n_20000.npz" ] || [ ! -f "data/test/duffing_monte_carlo_trajectories_dv_${DV_ood}_dt_0.02_n_20000_ood.npz" ]; then
+    echo "Generating dataset for the Duffing system..."
+    python scripts/datagen/duffing_delta_v_gen.py --dv ${DV} --dt 0.02 --n 20000 --ood
+    echo "Dataset generation complete."
+else
+    echo "Dataset already exists. Skipping generation."
+fi
+
+
+python scripts/reachabilityDuffing.py --train-ratio 0.2 --model mamba --dv ${DV}
+python scripts/reachabilityDuffing.py --train-ratio 0.8 --model mamba --dv ${DV}
+python scripts/reachabilityDuffing.py --train-ratio 0.2 --model mamba --ood --dv ${DV}
+python scripts/reachabilityDuffing.py --train-ratio 0.8 --model mamba --ood --dv ${DV}
+
+python scripts/reachabilityDuffing.py --train-ratio 0.2 --model lstm --dv ${DV}
+python scripts/reachabilityDuffing.py --train-ratio 0.8 --model lstm --dv ${DV}
+python scripts/reachabilityDuffing.py --train-ratio 0.2 --model lstm --ood --dv ${DV}
+python scripts/reachabilityDuffing.py --train-ratio 0.8 --model lstm --ood --dv ${DV}
